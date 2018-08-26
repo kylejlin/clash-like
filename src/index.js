81387eacafed21b6c28d84c3676f1fbc54d47db7
Kyle Lin <kylejlin.us@gmail.com>
2018-08-25 19:26:00 -0700

    Make it sort of work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
+// const camera = new THREE.PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   1e5,
+// );
+const camera = new THREE.OrthographicCamera(
+  window.innerWidth / -2,
+  window.innerWidth / 2,
+  window.innerHeight / 2,
+  window.innerHeight / -2,
   1,
   1e5,
 );
@@ -45,8 +53,118 @@ for (let x = 0; x < 10; x++) {
   }
 }
 
+const hitbox = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(
+    1e9,
+    2,
+    1e9,
+  ),
+  new THREE.MeshStandardMaterial({ color: 0xff0000 }),
+);
+hitbox.position.set(0, 0, 0);
+hitbox.material.transparent = true;
+hitbox.material.opacity = 0;
+world.add(hitbox);
+
 const render = () => {
   renderer.render(scene, camera);
 };
 
 render();
+
+const clientToNDC = (x, y) => [
+  2 * x / window.innerWidth - 1,
+  -(2 * y / window.innerHeight - 1),
+];
+
+const lastTouches = {};
+const updateLastTouches = (touchList) => {
+  Array.from(touchList).forEach((touch) => {
+    const [x, y] = clientToNDC(touch.clientX, touch.clientY);
+    lastTouches[touch.identifier] = [x, y];
+  });
+};
+
+window.addEventListener('touchstart', (e) => {
+  updateLastTouches(e.changedTouches);
+});
+window.addEventListener('touchmove', (e) => {
+  e.preventDefault();
+  if (e.touches.length === 1) {
+    const [x, y] = clientToNDC(e.touches[0].clientX, e.touches[0].clientY);
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+    const hits = raycaster.intersectObject(hitbox);
+    if (hits.length === 0) {
+      return;
+    }
+    const [{ point }] = hits;
+    const [oldX, oldY] = lastTouches[e.touches[0].identifier];
+    raycaster.setFromCamera(new THREE.Vector2(oldX, oldY), camera);
+    const oldHits = raycaster.intersectObject(hitbox);
+    if (oldHits.length === 0) {
+      return;
+    }
+    const [{ point: oldPoint }] = oldHits;
+    const delta = point.clone().sub(oldPoint);
+    delta.y = 0;
+    world.position.add(delta);
+  } else if (e.touches.length === 2) {
+    try {
+      const raycaster = new THREE.Raycaster();
+      const [x0, y0] = clientToNDC(e.touches[0].clientX, e.touches[0].clientY);
+      raycaster.setFromCamera(new THREE.Vector2(x0, y0), camera);
+      const hits0 = raycaster.intersectObject(hitbox);
+      if (hits0.length === 0) {
+        return;
+      }
+      const [{ point: p0 }] = hits0;
+      const [ox0, oy0] = lastTouches[e.touches[0].identifier];
+      raycaster.setFromCamera(new THREE.Vector2(ox0, oy0), camera);
+      const oldHits0 = raycaster.intersectObject(hitbox);
+      if (oldHits0.length === 0) {
+        return;
+      }
+      const [{ point: op0 }] = oldHits0;
+      p0.y = 0;
+      op0.y = 0;
+      const [x1, y1] = clientToNDC(e.touches[1].clientX, e.touches[1].clientY);
+      raycaster.setFromCamera(new THREE.Vector2(x1, y1), camera);
+      const hits1 = raycaster.intersectObject(hitbox);
+      if (hits1.length === 0) {
+        return;
+      }
+      const [{ point: p1 }] = hits1;
+      const [ox1, oy1] = lastTouches[e.touches[1].identifier];
+      raycaster.setFromCamera(new THREE.Vector2(ox1, oy1), camera);
+      const oldHits1 = raycaster.intersectObject(hitbox);
+      if (oldHits1.length === 0) {
+        return;
+      }
+      const [{ point: op1 }] = oldHits1;
+      p1.y = 0;
+      op1.y = 0;
+      const dist = p0.clone().sub(p1).length();
+      const oldDist = op0.clone().sub(op1).length();
+      const scale = dist / oldDist;
+
+      const oldMp = p0.clone().add(p1).multiplyScalar(0.5);
+      const offset = oldMp.clone().sub(world.position).multiplyScalar(scale);
+      const newMp = world.position.clone().add(offset);
+      const displacement = newMp.clone().sub(oldMp);
+
+      world.scale.multiplyScalar(scale);
+      world.position.sub(displacement);
+    } catch (error) {
+      alert('error');
+      alert(error);
+    }
+  }
+  updateLastTouches(e.changedTouches);
+  render();
+}, { passive: false });
+window.addEventListener('touchend', (e) => {
+  Array.from(e.changedTouches).forEach((touch) => {
+    delete lastTouches[touch.identifier];
+  });
+});
